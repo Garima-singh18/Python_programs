792925f680edf44e953e34e1e4d0811c21a3928f
Garima singh <singhgarima5863@gmail.com>
2026-06-26 16:05:40 +0530

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,15 @@
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+
+Twinkle Twinkle Little Star
+Shining brightly and afar
+Twinkle stardust all around
+From the sky right to the ground
+Twinkle Twinkle Little Star
+Shining brightly and afar
+You might also like''')
+
