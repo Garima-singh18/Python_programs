4fdeb6b3c049e13c8fecd9a9881d1d01d5fe4f13
Garima singh <singhgarima5863@gmail.com>
2026-06-26 16:06:37 +0530

    Add files via upload

---
+++ _list.py
@@ -0,0 +1,22 @@
+L1 = [18,16,9,13,2]
+
+L1.sort()
+print(L1)
+L1.reverse()
+print(L1)
+L1.append(28)
+print(L1)
+L1.insert(4,44)
+print(L1)
+value = L1.pop(3)
+print(value)
+L1.remove(18)
+print(L1)
+
+
+movies = []
+movies.append(input("Enter 1st movie:"))
+movies.append(input("Enter 2nd movie:"))
+movies.append(input("Enter 3rd movies:"))
+
+print(movies)
