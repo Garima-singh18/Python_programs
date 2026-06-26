fd325047bef0c51a7826e2e22aad13f29125fbbb
Garima singh <singhgarima5863@gmail.com>
2026-06-26 16:11:48 +0530

    Add files via upload

---
+++ tup.py
@@ -0,0 +1,12 @@
+tup = (2,1,3,1)
+print(tup[0])
+print(tup[1])
+
+
+grade = ["C","D","A","A","B","B","A"]
+print(grade.count("A"))
+
+
+grade = ["C","D","A","A","B","B","A"]
+grade.sort()
+print(grade)
